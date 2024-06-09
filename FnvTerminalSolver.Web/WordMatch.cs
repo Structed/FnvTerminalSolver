cf6c70d94776838a7b0561c4b5619e1638339b13
Johannes Ebner <Structed@users.noreply.github.com>
2024-06-09 21:16:29 +0200

    Improved output with deduplication and color-coding

---
+++ FnvTerminalSolver.Web/WordMatch.cs
@@ -0,0 +1,8 @@
+﻿namespace FnvTerminalSolver.Web;
+
+public record WordMatch(string WordA, string WordB, char[] CommonCharacters)
+{
+    public string WordA { get; set; } = WordA;
+    public string WordB { get; set; } = WordB;
+    public char[] CommonCharacters { get; set; } = CommonCharacters;
+}
